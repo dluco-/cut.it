c4fabd2b493756fc2ce0450667b9b370a33c7b89
dluco- <dennis.olsson@spinit.se>
2016-12-03 11:48:31 +0100

    Added angularfire2 and firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any[]>;
+  sizeSubject: Subject<any>;
+
+  constructor(public af: AngularFire) {
+    this.sizeSubject = new Subject();
+    this.items = af.database.list('/items', {
+      query: {
+        orderByChild: 'size',
+        equalTo: this.sizeSubject
+      }
+    });
+  }
+  filterBy(size: string) {
+    this.sizeSubject.next(size);
+  }
+
+  login() {
+    this.af.auth.login();
+  }
+
+  logout() {
+    this.af.auth.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+const firebaseConfig = {
+  apiKey: 'AIzaSyCldq7MdtidOj08PmSIASm03805o2HLhAE',
+  authDomain: 'cutit-e32e8.firebaseapp.com',
+  databaseURL: 'https://cutit-e32e8.firebaseio.com',
+  storageBucket: 'cutit-e32e8.appspot.com',
+  messagingSenderId: '260838937829'
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +27,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
