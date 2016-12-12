eed5c006b7bb9a0d1881e20e426213872309b2d4
dluco- <dennis.olsson@spinit.se>
2016-12-12 20:42:36 +0100

    Added semantic-ui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,32 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 import { Location } from '@angular/common';
 
-
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  location: Location;
+    location: Location;
 
-  constructor(public af: AngularFire, location: Location) {
-    // this.af.auth.subscribe(auth => console.log(auth));
-  }
+    constructor(public af: AngularFire, location: Location) {
+        // this.af.auth.subscribe(auth => console.log(auth));
+    }
 
-  loginGoogle() {
-    this.af.auth.login({
-      provider: AuthProviders.Google,
-      method: AuthMethods.Popup,
-    }).then(resp => {
-    }).catch(err => console.log(err));
-  }
+    loginGoogle(): void {
+        this.af.auth.login({
+            provider: AuthProviders.Google,
+            method: AuthMethods.Popup,
+        }).catch(err => console.log(err));
+    }
 
-  logout() {
-    this.af.auth.logout();
-  }
+    logout(): void {
+        this.af.auth.logout();
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
